18c828fa50ee6eb33146b5c8d65ddc63f13b478b
Ting <eeyore111.12@gmail.com>
2025-08-26 10:16:11 +0800

    新增ting分支,新增sidebar

---
+++ client/app/user/_components/sidebar.js
@@ -0,0 +1,24 @@
+'use client'
+
+import { useAuth } from '@/hooks/use-auth'
+import { useState } from 'react'
+import styles from './sidebar.module.css'
+import Link from 'next/link'
+
+export default function UserSidebarPage() {
+    return (
+    <div className="container-fluid">
+        <div className="row">
+        {/* 左側功能列 */}
+            <div className={`col-md-3 ${styles.sidebar}`}>
+                <a href="#">我的資料</a>
+                <a href="#">訂單查詢</a>
+                <a href="#">我的優惠券</a>
+                <a href="#">我的最愛</a>
+                <a href="#">收藏文章</a>
+                <a href="#">預約紀錄</a>
+            </div>
+        </div>
+    </div>
+    )
+}
