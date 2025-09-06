807add893aa4137663775cbd0ca924b13c0e9759
Ting <eeyore111.12@gmail.com>
2025-09-06 11:42:45 +0800

    auth

--- client/app/user/page.js
+++ client/app/user/page.js
@@ -1,12 +1,16 @@
 'use client'
 
-import React, { useState, useEffect } from 'react'
-// import Link from 'next/link'
+import { useState, useEffect } from 'react'
+import Link from 'next/link'
 
 export default function UserPage(props) {
   return (
-    <>
-      <h1>會員管理中心</h1>
-    </>
+    <div className={styles.content}>
+      <link className='btn btn-primary me-1 mb-1' href="/login"> 登入</link>
+      <link className='btn btn-primary me-1 mb-1' href="/register">註冊(C)</link>
+      <link className='btn btn-primary me-1 mb-1' href="/user"> 使用者列表頁(R)</link>
+      <link className='btn btn-primary  me-1 mb-1' href="/user/ben">使用者的主頁(R)</link>
+      <link className='btn btn-primary  me-1 mb-1' href="/user/edit"> 修改個人資料(U)</link>
+    </div>
   )
 }
