2caf6e00b79afc955ef89d9b2aa73560ec650827
Chunny <ab880555@gmail.com>
2025-09-15 09:44:32 +0800

    0915 合併

--- server/connect.js
+++ server/connect.js
@@ -8,6 +8,14 @@ const pool = mysql.createPool({
   password: "a12345",
   database: "oakly"
 });
+// 學校電腦
+const pool = mysql.createPool({
+  host: "localhost",
+  port: 3306,
+  user: "admin",
+  password: "a12345",
+  database: "oakly"
+});
 
 
 // 雅嵐
@@ -18,6 +26,27 @@ const pool = mysql.createPool({
 //   password: "a12345",
 //   database: "oakly"
 // });
+
+
+// 雅嵐
+const pool = mysql.createPool({
+  host: 'localhost',
+  user: 'root',
+  password: '',
+  database: 'product',
+  waitForConnections: true,
+  connectionLimit: 10,
+  queueLimit: 0,})
+
+//欣錞
+// const pool = mysql.createPool({
+//   host: 'localhost',
+//   user: 'root',
+//   password: '',
+//   database: 'product',
+//   waitForConnections: true,
+//   connectionLimit: 10,
+//   queueLimit: 0,})
 
 
 // // 羿葶
