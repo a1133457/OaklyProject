a35a01c2cbe3ea145875f1c37aa2b1aa1575b25f
Chunny <ab880555@gmail.com>
2025-09-15 11:53:04 +0800

    0915 修改一些先上傳

--- server/connect.js
+++ server/connect.js
@@ -1,13 +1,13 @@
 import mysql from "mysql2/promise";
 
 // 學校電腦
-// const pool = mysql.createPool({
-//   host: "localhost",
-//   port: 3306,
-//   user: "admin",
-//   password: "a12345",
-//   database: "oakly"
-// });
+const pool = mysql.createPool({
+  host: "localhost",
+  port: 3306,
+  user: "admin",
+  password: "a12345",
+  database: "oakly"
+});
 
 
 // 雅嵐
@@ -18,17 +18,6 @@ import mysql from "mysql2/promise";
 //   password: "a12345",
 //   database: "oakly"
 // });
-
-
-// 雅嵐
-// const pool = mysql.createPool({
-//   host: 'localhost',
-//   user: 'root',
-//   password: '',
-//   database: 'product',
-//   waitForConnections: true,
-//   connectionLimit: 10,
-//   queueLimit: 0,})
 
 
 // // 羿葶
