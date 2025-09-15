cdff9492f77cb1ae1290dc602a180386c8850d87
Chunny <ab880555@gmail.com>
2025-09-15 11:03:10 +0800

    0915 因為登入有問題，先退回 merge ting 之前

--- server/connect.js
+++ server/connect.js
@@ -1,21 +1,13 @@
 import mysql from "mysql2/promise";
 
-// 學校電腦
-const pool = mysql.createPool({
-  host: "localhost",
-  port: 3306,
-  user: "admin",
-  password: "a12345",
-  database: "oakly"
-});
-// 學校電腦
-const pool = mysql.createPool({
-  host: "localhost",
-  port: 3306,
-  user: "admin",
-  password: "a12345",
-  database: "oakly"
-});
+// // 學校電腦
+// const pool = mysql.createPool({
+//   host: "localhost",
+//   port: 3306,
+//   user: "admin",
+//   password: "a12345",
+//   database: "oakly"
+// });
 
 
 // 雅嵐
@@ -29,14 +21,14 @@ const pool = mysql.createPool({
 
 
 // 雅嵐
-const pool = mysql.createPool({
-  host: 'localhost',
-  user: 'root',
-  password: '',
-  database: 'product',
-  waitForConnections: true,
-  connectionLimit: 10,
-  queueLimit: 0,})
+// const pool = mysql.createPool({
+//   host: 'localhost',
+//   user: 'root',
+//   password: '',
+//   database: 'product',
+//   waitForConnections: true,
+//   connectionLimit: 10,
+//   queueLimit: 0,})
 
 //欣錞
 // const pool = mysql.createPool({
